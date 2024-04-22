d8afb27bde35cf0440cdba1d49927a92df58a2c0
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2024-04-22 10:45:12 +0200

    Initial upload

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableWithoutFeedback, View, SafeAreaView, Image, Alert, Button, StatusBar, Platform } from 'react-native';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // SafeAreaView is only for iOS, for android use the platform module and add padding
+    <View
+    style={{
+      backgroundColor: "#fff",
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: clearInterval,
+    }}
+    >
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: 100,
+        height: 100,
+      }} />
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 100,
+      }} />
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100,
+      }} />
     </View>
   );
 }
@@ -14,7 +37,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
