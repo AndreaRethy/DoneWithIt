d581b66edc7e1ee2977070674abcbaf73d9d802d
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2024-04-22 11:10:48 +0200

    Create new paths

--- App.js
+++ App.js
@@ -1,44 +1,8 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableWithoutFeedback, View, SafeAreaView, Image, Alert, Button, StatusBar, Platform } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
 
-  return (
-    // SafeAreaView is only for iOS, for android use the platform module and add padding
-    <View
-    style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: clearInterval,
-    }}
-    >
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100,
-      }} />
-    </View>
-  );
+  return < WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
